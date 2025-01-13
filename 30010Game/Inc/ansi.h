42a33cc83400eb0c31a7d92dfc461353275be785
vinc0208 <s246154@dtu.dk>
2025-01-13 15:08:10 +0100

    Ansi.c & Ansi.h content

--- 30010Game/Inc/ansi.h
+++ 30010Game/Inc/ansi.h
@@ -1,13 +1,22 @@
-/*
- * ansi.h
- *
- *  Created on: 13 Jan 2025
- *      Author: vince
- */
-
-#ifndef ANSI_H_
-#define ANSI_H_
+#ifndef _ANSI_H_
+#define _ANSI_H_
+/* Includes -------------------------------------------------------------*/
+#include "stdio.h"
+#include "stdint.h"
 
 
+// Whatever needs to be included
+/* Exported types -------------------------------------------------------*/
+/* Exported constants ---------------------------------------------------*/
+/* Exported macro -------------------------------------------------------*/
 
-#endif /* ANSI_H_ */
+
+void reverse(uint8_t on);
+void underline(uint8_t on);
+void clrreol();
+void clrscr();
+void gotoxy(int x, int y);
+void fgcolor(uint8_t foreground);
+void bgcolor(uint8_t background);
+void color(uint8_t foreground, uint8_t background);
+#endif /* _ANSI_H_ */
